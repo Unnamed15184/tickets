2f2294116d168fb6b4d06366a3766203deac0a51
Adriano_Silva <adriannounnamed@gmail.com>
2025-04-28 22:44:52 -0300

    Add files via upload

---
+++ projeto1/src/app/tab2/tab2.page.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { SenhasService } from '../services/senhas.service';
+
+@Component({
+  selector: 'app-tab2',
+  templateUrl: 'tab2.page.html',
+  styleUrls: ['tab2.page.scss']
+})
+export class Tab2Page {
+
+  constructor(public senhasService: SenhasService) { }
+
+  atenderSenha(atendente: string) {
+    const senhaAtendida = this.senhasService.atenderSenha(atendente);
+
+    console.log("Senha atendida:", senhaAtendida);
+
+}
+}
